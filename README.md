a8c44c1fb8e5e57bd785ad2b0610294115b649d8
EricGebhart <e.a.gebhart@gmail.com>
2016-01-30 12:12:21 -0500

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # audio_cloze
 Anki addon that changes the behavior of audio inside clozes to be consistent with the textual behavior.
+
+
+The non visible clozes do not make any sound.  If an audio hint is desired for a cloze, place the audio in the hint for that cloze. Multiple sounds can be added to the answer or the hint.
+
+This addon allows for complete control over audio in notes which have multiple clozes.
+
---
+++ audio_cloze.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+
+# Audio Cloze.
+# Copyright (C) 2016  e.a.gebhart@gmail.com
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Name: Audio Cloze
+Filename: acloze.py
+Version: 0.1
+
+Author: Eric Gebhart
+Contact: e.a.gebhart at gmail.com
+Github: https://github.com/EricGebhart/audio_cloze.git
+
+Description: Changes the behavior of clozes which contain audio to be
+            consistent with the behavior of text within clozes. ie. The non
+            visible clozes do not make any sound.  If an audio hint is desired
+            for a cloze, place the audio in the hint for that cloze. Multiple
+            sounds can be added to the answer or the hint.
+"""
+
+from anki import template
+from anki.hooks import wrap
+
+import re
+
+soundReg = r"\[sound:.*\]"
+clozeReg = r"(?s)\{\{c%s::(.*?)(::(.*?))?\}\}"
+
+
+def removeSound(txt):
+    """remove sound tags from a string of text"""
+    return re.sub(soundReg, "", txt)
+
+
+def myClozeText(txt, ord, type):
+    """Render clozes for the current card,
+    remove sound from other clozes so they will not play."""
+    reg = clozeReg
+    if not re.search(reg % ord, txt):
+        return ""
+
+    def repl(m):
+        """replace chosen cloze with appropriate text"""
+        hint = (m.group(3) if m.group(3) else "")
+        answer = m.group(1)
+        hintWithOutSound = removeSound(hint)
+
+        # it's a question.
+        if type == "q":
+
+            # the hint has text and sound.
+            if hintWithOutSound:
+                return "<span class=cloze>[%s]</span>" % hint
+
+            # the hint only has sound or is completely empty.
+            else:
+                return "<span class=cloze>[...]%s</span>" % hint
+
+        # it's an answer.
+        else:
+            return "<span class=cloze>%s</span>" % answer
+
+    # replace all of this card's clozes with the hint or the answer.
+    txt = re.sub(reg % ord, repl, txt)
+
+    def repl2(m):
+        """remove the sound from a cloze answer"""
+        return removeSound(m.group(1))
+
+    # replace the rest of the clozes with their answers minus sound.
+    return re.sub(reg % "\d+", repl2, txt)
+
+
+def init_acloze(self, *args, **kwargs):
+    self.clozeText = myClozeText
+
+template.Template.clozeText = myClozeText
+template.Template.__init__ = wrap(template.Template.__init__, init_acloze)
